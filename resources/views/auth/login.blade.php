ba04564f87bd6392004b72eeda82c929b8972022
Spreethy <preethysrinevasan@gmail.com>
2026-07-29 09:56:06 +0530

    Add register link to login page, fix logout redirect

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,4 +44,13 @@
             </x-primary-button>
         </div>
     </form>
+
+    <div class="text-center mt-6">
+        <p class="text-sm text-gray-600">
+            Not registered?
+            <a href="{{ route('register') }}" class="text-indigo-600 hover:text-indigo-900 underline font-medium">
+                Create an account
+            </a>
+        </p>
+    </div>
 </x-guest-layout>
